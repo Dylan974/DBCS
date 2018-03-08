6950c5268c2a677cf18aa7827e4e3dffaeb09a21
unknown <mugidylan@gmail.com>
2018-03-08 16:59:05 +0400

    first commit. Add boilerplate

---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCRQu2msPQurxGX2ZGqNpRJPP5cR97pYEI',
+    authDomain: 'dbcs-61405.firebaseapp.com',
+    databaseURL: 'https://dbcs-61405.firebaseio.com',
+    projectId: 'dbcs-61405',
+    storageBucket: 'dbcs-61405.appspot.com',
+    messagingSenderId: '1057039024159'
+  }
+};
